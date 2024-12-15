5436e3f7059b77d79e7cab0cbcb44e1f05928ba6
Xiaozhe Cheng <xc200@duke.edu>
2024-12-14 22:25:10 -0500

    project created

---
+++ src/main/java/com/example/postreply/PostReplyApplication.java
@@ -0,0 +1,13 @@
+package com.example.postreply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostReplyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostReplyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/postreply/PostReplyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postreply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostReplyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
