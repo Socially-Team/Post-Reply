425866e1048646453ab0fa3d4a49a961f10183a7
Hon Ching <honspaceching@gmail.com>
2024-12-19 02:20:03 -0500

    don't have cors, since api gateway already cors, or else error

--- src/main/java/com/example/postreply/security/WebConfig.java
+++ src/main/java/com/example/postreply/security/WebConfig.java
@@ -1,29 +1,29 @@
-package com.example.postreply.security;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig  implements WebMvcConfigurer {
-
-//    /*passwordEncoder put here - to avoid circular dependencies between SecurityConfig and UserService*/
-//    @Bean
-//    public PasswordEncoder passwordEncoder(){
-//        return new BCryptPasswordEncoder();
+//package com.example.postreply.security;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig  implements WebMvcConfigurer {
+//
+////    /*passwordEncoder put here - to avoid circular dependencies between SecurityConfig and UserService*/
+////    @Bean
+////    public PasswordEncoder passwordEncoder(){
+////        return new BCryptPasswordEncoder();
+////    }
+//
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**") // Apply to all paths
+//                .allowedOrigins("http://localhost:5173") // Allow requests from Angular app
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH","OPTIONS") // Allowed HTTP methods
+//                .allowedHeaders("*") // Allow all headers
+//                .allowCredentials(true); // Allow cookies or credentials if needed
 //    }
-
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Apply to all paths
-                .allowedOrigins("http://localhost:5173") // Allow requests from Angular app
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH","OPTIONS") // Allowed HTTP methods
-                .allowedHeaders("*") // Allow all headers
-                .allowCredentials(true); // Allow cookies or credentials if needed
-    }
-}
-
+//}
+//
