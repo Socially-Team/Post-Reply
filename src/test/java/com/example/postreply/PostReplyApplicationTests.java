e1fbacdcfffadfaf26d5477783b6e6224845cdda
Hon Ching <honspaceching@gmail.com>
2024-12-21 23:18:19 -0500

    SCRUM-39 setup configurations for controller tests
    configurations including application-test.yml to setup the h2 database
    annotations configs in PostReplyApplicationTests

--- src/test/java/com/example/postreply/PostReplyApplicationTests.java
+++ src/test/java/com/example/postreply/PostReplyApplicationTests.java
@@ -1,9 +1,17 @@
 package com.example.postreply;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
+
+@ActiveProfiles("test")
 @SpringBootTest
+@MockBean(PostRepository.class)
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 class PostReplyApplicationTests {
 
     @Test
